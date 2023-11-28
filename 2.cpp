02509742e2ee273d25fcb778704dfd48d796aa23
agonzolm@myuax.com <agonzolm@myuax.com>
2023-11-28 12:16:23 +0100

    .

--- 2.cpp
+++ 2.cpp
@@ -1,7 +1,47 @@
-// Clase base para representar diferentes tipos de datos
+
 class VariantValue {
 public:
     virtual ~VariantValue() {}
     virtual std::string to_string() const = 0;
+};
+
+class VariantInt : public VariantValue {
+    // ... resto del código
+
+public:
+    std::string to_string() const override {
+        return "Integer: " + std::to_string(value);
+    }
+};
+
+class VariantString : public VariantValue {
+
+public:
+    std::string to_string() const override {
+        return "String: " + value;
+    }
+};
+
+class VariantList : public VariantValue {
+    // ... resto del código
+
+public:
+    std::string to_string() const override {
+        std::string result = "List: [";
+        for (const auto& elem : value) {
+            result += elem->to_string() + " ";
+        }
+        result += "]";
+        return result;
+    }
+};
+
+class VariantProcedure : public VariantValue {
+    // ... resto del código
+
+public:
+    std::string to_string() const override {
+        return "Procedure";
+    }
 };
 
