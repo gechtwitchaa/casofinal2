51d7b38e2da65e0a0f680bb719bf3a6baf97f4ef
agonzolm@myuax.com <agonzolm@myuax.com>
2023-11-28 12:11:05 +0100

    .

--- 1.cpp
+++ 1.cpp
@@ -12,3 +12,23 @@ public:
 class VariantInt : public VariantValue {
 private:
     int value;
+
+public:
+    VariantInt(int val) : value(val) {}
+
+    void print() const override {
+        std::cout << "Integer: " << value << std::endl;
+    }
+};
+
+class VariantString : public VariantValue {
+private:
+    std::string value;
+
+public:
+    VariantString(const std::string& val) : value(val) {}
+
+    void print() const override {
+        std::cout << "String: " << value << std::endl;
+    }
+};
